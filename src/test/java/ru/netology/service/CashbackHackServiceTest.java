9bfe7c56a25019002c30a7066bb484bc7141ee30
Natalia <natysun07@rambler.ru>
2023-06-14 21:17:54 +0300

    Added new tests with jUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void IfAmountLess1_000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assert.assertEquals (expected, actual);
+    }
+
+    @Test
+    public void IfAmountEqually1_001() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void IfAmountEqually0() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void IfAmountEqually1_000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
+
+}
