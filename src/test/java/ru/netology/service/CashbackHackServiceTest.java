82562188502268fdfcb654618a3e595d67abc781
Natalia <natysun07@rambler.ru>
2023-06-14 20:35:43 +0300

    Add new tests with TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    void IfAmountLess1_000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    void IfAmountEqually1_001() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        Assert.assertEquals(actual, expected);
+
+
+    }
+
+    @Test
+    void IfAmountEqually0() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+}
